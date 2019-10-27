b4adc17f2e6814a99c174a5fc2f1f1a229868f99
Måns Isaksson <mans.95.isaksson@gmail.com>
2019-10-27 16:21:53 +0100

    added deg_to_rad

--- src/core/math.h
+++ src/core/math.h
@@ -13,6 +13,12 @@ namespace math
 		return 3.14159265359f;
 	}
 
+	template<typename U>
+	inline U deg_to_rad(const U& deg)
+	{
+		return deg * 0.0174532925f;
+	}
+
 	/*bool nearly_equals(float a, float b, float epsilon = 0.001)
 	{
 		return fabs(a - b) < epsilon;
