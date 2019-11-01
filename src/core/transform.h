33478e619178357e37d0f256d9ebd57f815374a6
mansisaksson <mans.95.isaksson@gmail.com>
2019-11-01 14:41:12 +0100

    Added to_matrix

--- src/core/transform.h
+++ src/core/transform.h
@@ -24,13 +24,37 @@ public:
 
 	static const transform identity;
 
-	inline matrix4x4 to_matrix() const
+	matrix4x4 to_matrix() const
 	{
-		matrix4x4 out_matrix = matrix4x4::identity;
+		const matrix4x4 translation_matrix =
+		{
+			{ 1, 0, 0, location.x },
+			{ 0, 1, 0, location.y },
+			{ 0, 0, 1, location.z },
+			{ 0, 0, 0, 1 },
+		};
 
-		
+		const auto forward = rotation.get_forward();
+		const auto right = rotation.get_right();
+		const auto up = rotation.get_up();
 
-		return out_matrix;
+		const matrix4x4 rotation_matrix =
+		{
+			{ forward.x,	forward.y,	forward.z,	0 },
+			{ right.x,		right.y,	right.z,	0 },
+			{ up.x,			up.y,		up.z,		0 },
+			{ 0,			0,			0,			1 }
+		};
+
+		const matrix4x4 scale_matrix =
+		{
+			{ scale,	0,		0,		0 },
+			{ 0,		scale,	0,		0 },
+			{ 0,		0,		scale,	0 },
+			{ 0,		0,		0,		1 },
+		};
+
+		return translation_matrix * rotation_matrix * scale_matrix;
 	}
 
 };
